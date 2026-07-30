f28c2704e0a18f523f0cbd1e645c2032f5537b84
Mohit Sharma <skmohit9955@gmail.com>
2026-07-30 22:29:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function isSameType(value1, value2) {
   //your js code here
 	if(typeof value1 === typeof value2){
+		return false
+	}
+	else if(typeof value1 === typeof value2){
 		return true
 	}else{
 		return false
